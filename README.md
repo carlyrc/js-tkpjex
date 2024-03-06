3e165ec2e1a03da218a22ac1e9bc7920e0f0426e
carlyrc <114524869+carlyrc@users.noreply.github.com>
2024-03-06 16:14:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-tkpjex
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/carlyrc/js-tkpjex)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+//Gráfico de columnas. Mi 1o gráfico
+
+var visualization = d3plus.viz()
+.container("#viz1")
+.data([{"year": 1991, "name":"alpha", "value": 30},
+       {"year": 1992, "name":"alpha", "value": 20},])
+.type("bar")
+.id("name")
+.x("year")
+.y("value")
+.draw()    
